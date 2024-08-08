bc669b5134a8040c8da7f08f4285fa6e0684a84f
DavidChavez513 <jose.david@nuclea.solutions>
2024-08-08 16:31:08 -0600

    Feat: Modificar cuando el servidor envia imagenes y cuando debemos enviar al usuario

--- functions/index.js
+++ functions/index.js
@@ -9,24 +9,56 @@ exports.imageGeneratedWebhook = onRequest(async (req, res) => {
     res.status(405).send('Method Not Allowed');
     return;
   }
-  
+
   try {
     const webhookData = req.body;
+
     // Validate the incoming data
-    if (!webhookData.order_id || !webhookData.image) {
+    if (!webhookData.order_id) {
       res.status(400).send('Invalid webhook data');
       return;
     }
-    
-    // Store the webhook data in Firestore
-    await firestore.collection('generatedImages').doc(webhookData.order_id).set(webhookData);
-    
-    // Process the webhook data (you can add more logic here)
-    console.log(`Processed image generation for order: ${webhookData.order_id}`);
-    console.log(`Generated image URL: ${webhookData.image.after_src}`);
-    
-    // Send a success response
-    res.status(200).send('Webhook received and processed successfully');
+
+    // Store the webhook data in Firestore, upload data && metadata image
+    if (webhookData.image) {
+      try {
+        
+        await firestore.collection('generatedImages').doc(webhookData.order_id).set(webhookData);
+
+        // Process the webhook data (you can add more logic here)
+        console.log(`Processed image generation for order: ${webhookData.order_id}`);
+        console.log(`Generated image URL: ${webhookData.image.after_src}`);
+
+        // Send a success response
+        res.status(200).send('Webhook received and processed successfully');
+
+      } catch (error) {
+
+        console.error(error)
+        res.status(400).send({
+          message: 'Ocurrio un error, intentalo de nuevo',
+          error
+        })
+      }
+    }
+
+    // Request to send user image
+    try {
+      const docImage = await firestore.collection('generatedImages').doc(webhookData.order_id).get()
+
+      const metadataImage = docImage.get('image')
+
+      res.status(200).send({
+        image_url: metadataImage.after_src
+      })
+    } catch (error) {
+      console.error(error)
+      res.status(400).send({
+        message: 'Ocurrio un error, intentalo de nuevo',
+        error
+      })
+    }
+
   } catch (error) {
     console.error('Error processing webhook:', error);
     res.status(500).send('Internal Server Error');
